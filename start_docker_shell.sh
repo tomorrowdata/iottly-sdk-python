23883459054fb2617bcb478f824eb4cba71ba79d
Stefano Terna <stefano.terna@tomorrowdata.io>
2018-02-14 13:26:39 +0100

    WIP testing sdk with an agent installer

--- start_docker_shell.sh
+++ start_docker_shell.sh
@@ -4,3 +4,9 @@
 docker build -f Dockerfile-py3-shell -t iottly-sdk-shell .
 
 docker run -it -v `pwd`/examples:/mybeautifulapp iottly-sdk-shell sh
+
+#docker run -it  \
+#  -v `pwd`/iottlyagent_1.6.0_linux_AMD64.tar.gz:/tmp/iottlyagent_1.6.0_linux_AMD64.tar.gz \
+#  -v `pwd`/examples:/mybeautifulapp \
+#  iottly-sdk-shell \
+#  sh
