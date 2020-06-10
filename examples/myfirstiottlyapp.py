6a835e919c4b4b08c99887b3e8fc092321ed7a54
Oscar-Campo <oscar.campolmi@hotmail.it>
2020-06-10 09:54:11 +0200

    Refactor example style

--- examples/myfirstiottlyapp.py
+++ examples/myfirstiottlyapp.py
@@ -32,17 +32,21 @@ def on_connection_status_changed(status):
     print('on_connection_status_changed: {}'.format(status))
 
 # Create an instance of IottlySDK with:
-# -  a "name" to identify your application (this will appear in the dashboard logs)
-# -  the optional number of messages retained if agent is down (defaults to 1500)
-# -  the previously defined callbacks (optional) to receive agent status notifications
+# -  a "name" to identify your application (this will appear
+#    in the dashboard logs)
+# -  the optional number of messages retained if agent is
+#    down (defaults to 10)
+# -  the previously defined callbacks (optional) to receive
+#    agent status notifications
 iottlysdk = IottlySDK(
-    name  = 'myfirstiottlyapp',
-    max_buffered_msgs = 100,
-    on_agent_status_changed = on_agent_status_changed,
-    on_connection_status_changed = on_connection_status_changed)
+    name='myfirstiottlyapp',
+    max_buffered_msgs=100,
+    on_agent_status_changed=on_agent_status_changed,
+    on_connection_status_changed=on_connection_status_changed)
 
-# Define one callback for each incoming commands you want to subscribe to.
-# Commands are defined in the iottly dashboard / management commands panel.
+# Define one callback for each incoming commands you want to
+# subscribe to. Commands are defined in the iottly
+# dashboard / management commands panel.
 def on_echo_received(cmdpars):
     print('on_echo_received: {}'.format(cmdpars))
 
@@ -50,23 +54,33 @@ def on_examplecommand_received(cmdpars):
     print('on_examplecommand_received: {}'.format(cmdpars))
 
 # Subscribe commands of interest and associate a callback
-iottlysdk.subscribe(cmd_type='echo', callback=on_echo_received)
-iottlysdk.subscribe(cmd_type='examplecommand', callback=on_examplecommand_received)
+iottlysdk.subscribe(
+    cmd_type='echo',
+    callback=on_echo_received
+)
+iottlysdk.subscribe(
+    cmd_type='examplecommand',
+    callback=on_examplecommand_received
+)
 
 # Start the sdk loops
 iottlysdk.start()
 
-# Here follows the main blocking loop of your application:
-# It "reads a temperature" and send it to iottly using the 'send' method.
+# Here follows the main blocking loop of your application.
+# It "reads a temperature" and send it to iottly using
+# the 'send' method.
 while True:
     try:
-        s = input('\n^C to exit, "m" to send 1 message, "l" to send 20 messages:\n')
+        s = input(
+            '\n^C to exit, "m" to send 1 message, '
+            '"l" to send 20 messages:\n'
+        )
         if s == 'm':
             t = {'temperature': 22}
             # send a message to iottly
             iottlysdk.send(t)
         if s == 'l':
-            for t in range(10,30):
+            for t in range(10, 30):
                 t = {'temperature': t}
 
                 # send a message to iottly
